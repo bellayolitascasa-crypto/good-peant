c02c6b5bf57157d02078c87d521e57dcf1526319
bellayolitascasa-crypto <bellayolitascasa@gmail.com>
2026-06-14 09:59:12 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,9 @@ const ASSETS = [
   "./styles.css",
   "./app.js",
   "./manifest.webmanifest",
-  "./icons/icon.svg"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png"
 ];
 
 self.addEventListener("install", (event) => {
